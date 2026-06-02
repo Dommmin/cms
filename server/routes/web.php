be2ec202a706880f2f5802faa00c8e6e4f8e4c87
Dominik <d.jasinski@ideosoftware.com>
2026-06-02 14:12:02 +0200

    feat: add route for glitchtip test

--- server/routes/web.php
+++ server/routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\HealthCheckController;
 use App\Http\Controllers\SeoController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-glitchtip', function () {
+    throw new Exception('Test GlitchTip error!');
+});
+
 Route::get('/feed', BlogFeedController::class)->name('blog.feed');
 Route::get('/robots.txt', [SeoController::class, 'robots'])->name('robots.txt');
 Route::get('/health', HealthCheckController::class)->name('health');
